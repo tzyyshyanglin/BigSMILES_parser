8957b5e97055b25fe94b4f527a1b0d7ea4fa3342
tzyyshyanglin <ts.lin.92@gmail.com>
2019-11-07 17:08:22 -0500

    change

--- new_bond_desc/BigSMILES_BigSmilesObj.py
+++ new_bond_desc/BigSMILES_BigSmilesObj.py
@@ -471,7 +471,7 @@ if __name__ == "__main__":
     ##testStr = '($1)-C\C=C/C(C)-($2)'
     #testStr = '{$=2CCO$1,$2CO$1}'
     testStr = '[$]{[$]CCCC[<1],CCC(C)C1CC1[<1]}C([>2])([<1])c1ccccc1'
-    testStr = '{[][<][Si](CC)(CC)O[>][]}'
+    testStr = '{[][<][Si](CC)(CC)O[>][$]}'
     #testStr = '{[][$]C\C=C/C[$][]}'
     
     #pattern = BigSmilesPattern._BSchainObjElement
